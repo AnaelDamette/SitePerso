d3656aedbee928b8d6b7004a629aadbb48cb0a64
Anael Damette <damette.anael@gmail.com>
2021-04-27 15:27:34 +0200

    Mise à jour Mockup

--- siteperso/src/components/TextePresentation.vue
+++ siteperso/src/components/TextePresentation.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="">
     <p class=" text-primaire text-center text-lg-left bg-dark p-2">
-      Développeur Web Fullstack Junior, je suis à votre disposition pour
+      Développeur Web Fullstack, je suis à votre disposition pour
       répondre à tout type de projet de création de sites internet ou
       d'applications Web.
     </p>
